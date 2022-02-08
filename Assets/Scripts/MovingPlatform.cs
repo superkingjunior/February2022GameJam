9da63edc25683fea0bfab40061719dae2edb7da3
Basel <baelzatahry@davidson.edu>
2022-02-08 12:39:02 +0100

    moving platform

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -4,12 +4,9 @@ using UnityEngine;
 
 public class MovingPlatform : MonoBehaviour
 {
-    public Rigidbody2D myRb2D;
-
+    Rigidbody2D myRb2D;
     Vector2 vel;
 
-    // the time platform would take to move in one direction
-    public float timer = 1;
     // Start is called before the first frame update
     void Start()
     {
@@ -26,39 +23,22 @@ public class MovingPlatform : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        timer -= Time.deltaTime;
         vel = myRb2D.velocity;
-        Vector2 normalizedDir = myRb2D.velocity.normalized;
-        if(transform.position.x < -2){
-            vel.x = -vel.x;
-            myRb2D.velocity = vel;
-        }
-        if(transform.position.x > 2){
-            vel.x = -vel.x;
-            myRb2D.velocity = vel;
-        }
-        if(transform.position.y < -3){
-            vel.y = -vel.y;
-            myRb2D.velocity = vel;
-        }
-        if(transform.position.y > 3){
-            vel.y = -vel.y;
-            myRb2D.velocity = vel;
-        }
         AboutToHit();
         
     }
 
     void AboutToHit(){
         // check below
-        Vector2 origin = this.transform.position+ new Vector3(0,-0.3f,0);
+        var size = transform.lossyScale;
+        Vector2 origin = this.transform.position+ new Vector3(0,-size.y/2,0);
 		Vector2 targetDown = this.transform.position + new Vector3(0,-2f,0);
 		Vector2 directionDown = targetDown - origin;
 		RaycastHit2D hitDown = Physics2D.Raycast(origin, directionDown, directionDown.magnitude);
         Debug.DrawRay(origin,directionDown,Color.red, 10f);
 
         // check above
-        origin = this.transform.position+ new Vector3(0,0.3f,0);
+        origin = this.transform.position+ new Vector3(0,size.y/2,0);
 		Vector2 targetUp = this.transform.position + new Vector3(0,2f,0);
 		Vector2 directionUp = targetUp - origin;
 		RaycastHit2D hitUp = Physics2D.Raycast(origin, directionUp, directionUp.magnitude);
@@ -88,18 +68,5 @@ public class MovingPlatform : MonoBehaviour
 		}
 
     }
-    void OnCollisionEnter2D(Collision2D col){
-        if(col.gameObject.CompareTag("Button")){
-            // A line that updates flapping force of obj in case of bouncing
-            print("Collision with dynamic platform");
-        }
-        else{
-            vel.x = -vel.x;
-            vel.y = -vel.y;
-            myRb2D.velocity = vel;
-            print("moving vs collision");
-        }
-    }
-
     
 }
