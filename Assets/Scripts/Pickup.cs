a815b09562b27b2af8e45521b68bc15878634cd6
Collin Presser <pressercc@gmail.com>
2022-02-08 12:42:47 +0100

    WASD should work now

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -18,7 +18,7 @@ public class Pickup : MonoBehaviour
         
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Button"))
         {
