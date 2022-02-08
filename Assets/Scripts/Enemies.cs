e22b8ace7275ddc416e94738d052f96b3a8983db
Basel <baelzatahry@davidson.edu>
2022-02-08 13:35:50 +0100

    Enemies working

--- Assets/Scripts/Enemies.cs
+++ Assets/Scripts/Enemies.cs
@@ -7,13 +7,12 @@ public class Enemies : MonoBehaviour
     public Rigidbody2D myRb2D;
 
     Vector2 vel;
-    float velX = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         myRb2D = GetComponent<Rigidbody2D>();
-  
+        var velX = 0;
         while(velX==0){
         velX = Random.Range(-1,1);}
         myRb2D.velocity = new Vector2(velX,0);
@@ -23,20 +22,7 @@ public class Enemies : MonoBehaviour
     void Update()
     {
         vel = myRb2D.velocity;
-        // makes sure enemy don't go out of bounds
-        if(transform.position.x < -7){
-            vel.x = -vel.x;
-        }
-        else if(transform.position.x > 7){
-            vel.x = -vel.x;
-        }
-        // if enemy is getting off platform then switch direction
-        if(!OnPlatform()){
-            var posX = new Vector3(transform.position.x -0.1f,transform.position.y, 0);
-            
-            vel.x = -vel.x;
-        }
-        myRb2D.velocity = vel;
+        OnPlatform();   
     }
 
     void OnCollisionEnter2D(Collision2D col){
@@ -53,18 +39,29 @@ public class Enemies : MonoBehaviour
         }
     }
     // Sends a raycast below to see if enemy is on the platform
-    bool OnPlatform()
+    void OnPlatform()
      {
-        Vector2 origin = this.transform.position+ new Vector3(0,-0.8f,0);
+        var size = transform.localScale;
+        Vector2 origin = this.transform.position+ new Vector3(0,-size.y,0);
 		Vector2 target = this.transform.position + new Vector3(0,-1.5f,0);
 		Vector2 direction = target - origin;
 		RaycastHit2D hit = Physics2D.Raycast(origin, direction, direction.magnitude);
-        //Debug.DrawRay(origin,direction,Color.red, 10f);
+        Debug.DrawRay(origin,direction,Color.red, 10f);
 		if(hit.collider!= null){
-            //print("on platform");
-			return true;
+            print("on platform");
+            
 		}
-        print("off platform");
-        return false;
+        else{
+            var pos = new Vector3(transform.position.x,transform.position.y, 0);
+            if(vel.x > 0){
+                pos.x = transform.position.x -0.1f;
+            }
+            else{
+                pos.x = transform.position.x +0.1f;
+            }
+            transform.position = pos;
+            vel.x=-vel.x;
+            myRb2D.velocity = vel;
+        }
      }
 }
