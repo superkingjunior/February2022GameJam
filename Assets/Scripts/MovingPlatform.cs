26c1362d5356033a7b44249e351fccabafb1d68a
Basel <baelzatahry@davidson.edu>
2022-02-08 12:44:32 +0100

    moving platform right and left

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -12,12 +12,9 @@ public class MovingPlatform : MonoBehaviour
     {
         myRb2D = GetComponent<Rigidbody2D>();
         var velX =0;
-        var velY =0;
         while(velX==0){
         velX = Random.Range(-1,1);}
-        while(velY==0){
-        velY = Random.Range(-1,1);}
-        myRb2D.velocity = new Vector2(velX,velY);
+        myRb2D.velocity = new Vector2(velX,0);
     }
 
     // Update is called once per frame
@@ -29,40 +26,22 @@ public class MovingPlatform : MonoBehaviour
     }
 
     void AboutToHit(){
-        // check below
-        var size = transform.lossyScale;
-        Vector2 origin = this.transform.position+ new Vector3(0,-size.y/2,0);
-		Vector2 targetDown = this.transform.position + new Vector3(0,-2f,0);
-		Vector2 directionDown = targetDown - origin;
-		RaycastHit2D hitDown = Physics2D.Raycast(origin, directionDown, directionDown.magnitude);
-        Debug.DrawRay(origin,directionDown,Color.red, 10f);
 
-        // check above
-        origin = this.transform.position+ new Vector3(0,size.y/2,0);
-		Vector2 targetUp = this.transform.position + new Vector3(0,2f,0);
-		Vector2 directionUp = targetUp - origin;
-		RaycastHit2D hitUp = Physics2D.Raycast(origin, directionUp, directionUp.magnitude);
-        Debug.DrawRay(origin,directionUp,Color.green, 10f);
 
         // check right
-        origin = this.transform.position+ new Vector3(1.7f,0,0);
+        Vector2 origin = this.transform.position+ new Vector3(1.7f,0,0);
 		Vector2 targetRight = this.transform.position + new Vector3(3,0,0);
 		Vector2 directionRight = targetRight - origin;
-		RaycastHit2D hitRight = Physics2D.Raycast(origin, directionRight, directionUp.magnitude);
+		RaycastHit2D hitRight = Physics2D.Raycast(origin, directionRight, directionRight.magnitude);
         Debug.DrawRay(origin,directionRight,Color.blue, 10f);
 
         // check left
         origin = this.transform.position+ new Vector3(-1.7f,0,0);
 		Vector2 targetLeft = this.transform.position + new Vector3(-3,0,0);
 		Vector2 directionLeft = targetLeft - origin;
-		RaycastHit2D hitLeft = Physics2D.Raycast(origin, directionLeft, directionUp.magnitude);
+		RaycastHit2D hitLeft = Physics2D.Raycast(origin, directionLeft, directionLeft.magnitude);
         Debug.DrawRay(origin,directionLeft,Color.yellow, 10f);
-		if(hitUp.collider!= null || hitDown.collider!=null){
-            vel.y = -vel.y;
-            myRb2D.velocity = vel;
-		}
-        else if(hitRight.collider!= null || hitLeft.collider!=null){
-            print("outta bounds");
+        if(hitRight.collider!= null || hitLeft.collider!=null){
             vel.x = -vel.x;
             myRb2D.velocity = vel;
 		}
